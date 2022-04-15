584314a547d3b5934e938223d8085ff4a7342bdf
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-15 22:23:24 +0300

    three

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ public class Main {
         One<Integer> A = new One<Integer>(123);
         One<String> B =  new One<>("Hello");
 
+        Two<Integer,String> C = new Two<>(1234,"Hello");
+
         System.out.println(A.getOb());
         System.out.println(B.getOb());
+        System.out.println(C.getT());
+        System.out.println(C.getV());
 
     }
 }
