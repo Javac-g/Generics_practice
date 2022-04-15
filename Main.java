52fd7a861008f57be6b7388170a832bb3023522e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-15 22:18:30 +0300

    one

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
-    public static void main(String...args){
 
+    public static void main(String...args){
 
         One<Integer> A = new One<Integer>(123);
         One<String> B =  new One<>("Hello");
 
         System.out.println(A.getOb());
+        System.out.println(B.getOb());
 
     }
 }
---
+++ Two.java
@@ -0,0 +1,4 @@
+public class Two<T,V> {
+    T t;
+    V v;
+}
