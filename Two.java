b666ba1de8b3c917867e497ce667fd1e8b98bd14
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-15 22:20:23 +0300

    two

--- Two.java
+++ Two.java
@@ -1,4 +1,18 @@
 public class Two<T,V> {
     T t;
     V v;
+
+    Two(T t, V v){
+
+        this.t = t;
+        this.v = v;
+
+    }
+
+    T getT(){
+        return t;
+    }
+    V getV(){
+        return v;
+    }
 }
