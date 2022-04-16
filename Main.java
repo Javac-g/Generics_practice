e939df9a339191e104454ac78a83e74577d2df3e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-16 17:09:44 +0300

    one G param and two

--- Main.java
+++ Main.java
@@ -1,19 +1,37 @@
 public class Main {
-    static void p(One<?> o){
-        System.out.println("Type: " + o.getClass().getName());
-        System.out.println("Value: " + o.getOb());
+    static void show_one(One<?> ob){
+        System.out.println("Class: " + ob.getClass().getName());
+        System.out.println("Value: " + ob.getOb());
+    }
+
+    static void show_two(Two<?,?> ob){
+        System.out.println("Class: " + ob.getClass().getName());
+        System.out.println("T Value: " + ob.getT());
+        System.out.println("V Value: " + ob.getV());
     }
     public static void main(String...args){
         Integer num = 4200;
         String str = "Hello world";
 
-        One<String> two = new One<>(str);
-        One<Integer> one = new One<>(num);
-        One raw = new One<>(404); //Raw type
 
-        p(two);
-        p(one);
-        p(raw);
+        One<Integer> oneA = new One<>(num);
+        One<String> oneB = new One<>(str);// Generic class 1 param.
+        One raw = new One<>(404); //Raw type.
+
+
+        Two<Integer,String> two = new Two<>(num,str);//Generic class with 2 param.
+
+        //output
+
+        show_one(oneB);
+        show_one(oneA);
+        show_one(raw);
+
+        show_two(two);
+
+
+
+
 
 
 
---
+++ Two.java
@@ -0,0 +1,24 @@
+public class Two<T,V> {
+    T t;
+    V v;
+
+    Two(T t, V v) {
+
+        this.t = t;
+        this.v = v;
+
+    }
+
+    public T getT() {
+        return t;
+    }
+
+
+    public V getV() {
+        return v;
+    }
+
+
+
+
+}
