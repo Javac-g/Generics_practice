ca854d579cb4a893882afe27618ec4279531d73c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-26 01:22:53 +0300

    App

---
+++ V1/App.java
@@ -0,0 +1,77 @@
+package V1;
+
+import sun.java2d.d3d.D3DDrawImage;
+
+public class App {
+
+    static void show_XY(Map<? extends XY> object){
+
+        System.out.println("--- [2D] ---");
+
+        for(int i = 0; i <= object.array.length - 1; i++){
+
+            System.out.print("[");
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y);
+            System.out.print("] , ");
+
+        }
+
+        System.out.println("\n------------");
+
+    }
+    static void show_XYZ(Map<? extends XYZ> object){
+
+        System.out.println("--- [3D] ---");
+
+        for(int i = 0 ; i <= object.array.length-1; i++){
+
+            System.out.print("[");
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z);
+            System.out.print("] , ");
+
+        }
+
+        System.out.println("\n------------");
+
+    }
+
+    static void show_XYZT(Map<? extends XYZT> object){
+
+        System.out.println("--- [4D] ---");
+
+        for (int i = 0; i <= object.array.length-1; i++){
+
+            System.out.print("[");
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z + " ");
+            System.out.print(object.array[i].t);
+            System.out.print("] , ");
+
+        }
+
+        System.out.println("\n------------");
+
+    }
+
+
+
+    public static void main(String...args){
+
+        XY[] D2 = {new XY(1,2), new XY(1,2), new XY(1,2)};
+        XYZ[] D3 = {new XYZ(1,2,3), new XYZ(1,2,3), new XYZ(1,2,3)};
+        XYZT[] D4 = {new XYZT(1,2,3,4),new XYZT(1,2,3,4), new XYZT(1,2,3,4)};
+
+        Map<XY> XY = new Map<>(D2);
+        Map<XYZ> XYZ = new Map<>(D3);
+        Map<XYZT> XYZT = new Map<>(D4);
+
+        show_XY(XY);
+        show_XYZ(XYZ);
+        show_XYZT(XYZT);
+
+    }
+}
