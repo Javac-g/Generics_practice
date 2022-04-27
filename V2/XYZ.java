6c7b54d46540ca39dfe99ab7ac1232a5ca389a20
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-27 22:26:31 +0300

    XYZT

--- V2/XYZ.java
+++ V2/XYZ.java
@@ -4,8 +4,11 @@ public class XYZ extends XY{
     int z;
 
     XYZ(int x, int y, int z){
+
         super(x,y);
+
         this.z = z;
+
     }
 
 }
---
+++ V2/XYZT.java
@@ -0,0 +1,13 @@
+package V2;
+
+public class XYZT extends XYZ{
+    int t;
+
+    XYZT(int x, int y,int z, int t){
+
+        super(x,y,z);
+        this.t = t;
+
+    }
+
+}
