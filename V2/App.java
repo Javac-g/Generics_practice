fb667c529914d8b3c3022946eb3b9f82b649aa2f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-27 22:54:17 +0300

    App

--- V2/App.java
+++ V2/App.java
@@ -15,29 +15,29 @@ public class App {
             System.out.print("[");
             System.out.print(object.array[i].x + " ");
             System.out.print(object.array[i].y);
-            System.out.println("]");
+            System.out.print("]  ");
         }
-        System.out.println("-------------------");
+
     }
 
     static void show_XYZ(Map<? extends XYZ> object){
 
-        System.out.println("---- [ 3 D ] ----");
+        System.out.println("\n---- [ 3 D ] ----");
 
         for(int i = 0 ; i <= object.array.length-1; i++){
 
             System.out.print("[");
             System.out.print(object.array[i].x + " ");
             System.out.print(object.array[i].y + " ");
-            System.out.println(object.array[i].z);
-            System.out.println("]");
+            System.out.print(object.array[i].z);
+            System.out.print("]  ");
         }
-        System.out.println("-------------------");
+
     }
 
     static void show_XYZT(Map<? extends XYZT> object){
 
-        System.out.println("---- [ 4 D ] ----");
+        System.out.println("\n---- [ 4 D ] ----");
 
         for(int i = 0 ; i <= object.array.length-1; i++){
 
@@ -45,10 +45,10 @@ public class App {
             System.out.print(object.array[i].x + " ");
             System.out.print(object.array[i].y + " ");
             System.out.print(object.array[i].z + " ");
-            System.out.println(object.array[i].t);
-            System.out.println("]");
+            System.out.print(object.array[i].t);
+            System.out.print("]  ");
         }
-        System.out.println("-------------------");
+
 
 
 
@@ -56,7 +56,17 @@ public class App {
 
     public static void main(String...args){
 
+        D2 = new XY[]{new XY(1, 2) , new XY(1,3)};
+        D3 = new XYZ[]{new XYZ(1,2,3), new XYZ(1,2,4)};
+        D4 = new XYZT[]{new XYZT(1,2,3,4), new XYZT(1,2,3,5)};
 
+        Map<XY> xy = new Map<>(D2);
+        Map<XYZ> xyz = new Map<>(D3);
+        Map<XYZT> xyzt = new Map<>(D4);
+
+        show_XY(xy);
+        show_XYZ(xyz);
+        show_XYZT(xyzt);
 
 
     }
