5a3fed3ef9c44edc1a2c26c8866e154d659579e0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-27 22:35:37 +0300

    show_xyz

--- V2/App.java
+++ V2/App.java
@@ -5,18 +5,36 @@ public class App {
     static XY[] D2;
     static XYZ[] D3;
     static XYZT[] D4;
+
     static void show_XY(Map<? extends XY> object){
 
         System.out.println("---- [ 2 D ] ----");
 
         for(int i = 0 ; i <= object.array.length-1; i++){
 
-            System.out.println("[");
-            System.out.println(object.array[i].x + " ");
-            System.out.println(object.array[i].y);
+            System.out.print("[");
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y);
             System.out.println("]");
         }
+        System.out.println("-------------------");
     }
+
+    static void show_XYZ(Map<? extends XYZ> object){
+
+        System.out.println("---- [ 3 D ] ----");
+
+        for(int i = 0 ; i <= object.array.length-1; i++){
+
+            System.out.print("[");
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y + " ");
+            System.out.println(object.array[i].z);
+            System.out.println("]");
+        }
+        System.out.println("-------------------");
+    }
+
     public static void main(String...args){
 
 
