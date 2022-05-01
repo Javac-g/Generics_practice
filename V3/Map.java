4af34a4c3ea09393d9448c7970dfc51b6ae73a88
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-02 00:03:06 +0300

    17

---
+++ V3/Map.java
@@ -0,0 +1,11 @@
+package V3;
+
+public class Map <T>{
+    T[] array;
+
+    Map(T[] array){
+
+        this.array = array;
+
+    }
+}
--- V3/XYZT.java
+++ V3/XYZT.java
@@ -4,8 +4,11 @@ public class XYZT extends XYZ{
     int t;
 
     XYZT(int x, int y, int z, int t){
+
         super(x,y,z);
+
         this.t = t;
+
     }
 
 }
