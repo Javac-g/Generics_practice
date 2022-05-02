5223bbffb77d4fae29060d489c2f330d12d080e0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-02 23:26:20 +0300

    2

---
+++ V4/High.java
@@ -0,0 +1,29 @@
+package V4;
+
+public class High {
+
+    int temp;
+
+    High(int temp){
+
+        this.temp = temp;
+
+    }
+
+    boolean isSame(High object){
+
+        return temp == object.getTemp();
+
+
+    }
+
+
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+}
