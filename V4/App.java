6b4278642da98ce49629f1ddf791358f54cfa4b5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-02 23:41:32 +0300

    12

--- V4/App.java
+++ V4/App.java
@@ -1,5 +1,7 @@
 package V4;
 
+import java.util.Random;
+
 public class App {
 
 
@@ -16,13 +18,14 @@ public class App {
 
     }
     public static void main(String...args){
-
+        Random r = new Random();
+        int num = r.nextInt(100);
         High[] days = {new High(10),new High(20),new High(30),
                        new High(40),new High(50),new High(60),
                        new High(70),new High(80),new High(90)
         } ;
 
-
+        System.out.println("Days is higer than: " + num + ": " + run(High::isBigger,days,new High(num)));
 
 
 
