c34c2ec8a1938b5d1e06eb114e212ccdb11db930
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-06 23:35:19 +0300

    9

--- V5/App.java
+++ V5/App.java
@@ -1,4 +1,18 @@
 package V5;
 
 public class App {
+
+    static void show_XY(Map<? extends XY> object){
+
+        for (int i = 0; i <= object.values.length-1; i++){
+
+            System.out.print(object.values[i].x + " ");
+
+            System.out.print(object.values[i].y + " ");
+
+
+        }
+
+        System.out.println();
+    }
 }
