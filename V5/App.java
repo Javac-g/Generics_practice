3d9e1fad4e499be4b94f325f43b649a5318abd40
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-06 23:37:44 +0300

    10

--- V5/App.java
+++ V5/App.java
@@ -15,4 +15,37 @@ public class App {
 
         System.out.println();
     }
+
+    static void show_XYZ(Map<? extends XYZ> object){
+
+        for (int i = 0; i <= object.values.length-1; i++){
+
+            System.out.print(object.values[i].x + " ");
+
+            System.out.print(object.values[i].y + " ");
+
+            System.out.print(object.values[i].z + " ");
+
+
+        }
+
+        System.out.println();
+    }
+
+
+    static void show_XYZT(Map<? extends XYZT> object){
+
+        for (int i = 0; i <= object.values.length-1; i++){
+
+            System.out.print(object.values[i].x + " ");
+
+            System.out.print(object.values[i].y + " ");
+
+            System.out.print(object.values[i].z + " ");
+
+            System.out.print(object.values[i].t + " ");
+        }
+
+        System.out.println();
+    }
 }
