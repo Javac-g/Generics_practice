b4332b579752e62abe5daf46d808a38b54e5f670
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 22:24:27 +0300

    5

---
+++ v8/App.java
@@ -0,0 +1,27 @@
+package v8;
+
+public class App {
+    static void show_XY(Map<?extends xy> object){
+        for (int i = 0; i <= object.array.length-1; i++){
+
+            System.out.print(object.array[i].x + " ");
+            System.out.println(object.array[i].y + " ");
+
+        }
+    }
+    public static void main(String...args){
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
