b94c81500b07643479bcae92b808cd3f74592f21
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 22:53:02 +0300

    7

--- v8/App.java
+++ v8/App.java
@@ -2,35 +2,38 @@ package v8;
 
 public class App {
     static void show_XY(Map<?extends xy> object){
+        System.out.println();
         for (int i = 0; i <= object.array.length-1; i++){
 
             System.out.print(object.array[i].x + " ");
-            System.out.println(object.array[i].y + " ");
+            System.out.print(object.array[i].y + " ");
 
         }
-        System.out.println();
+
     }
 
     static void show_XYZ(Map<?extends xyz> object){
+        System.out.println();
         for (int i = 0; i <= object.array.length-1; i++){
 
             System.out.print(object.array[i].x + " ");
-            System.out.println(object.array[i].y + " ");
-            System.out.println(object.array[i].z + " ");
+            System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z + " ");
 
         }
-        System.out.println();
+
     }
     static void show_XYZT(Map<?extends xyzt> object){
+        System.out.println();
         for (int i = 0; i <= object.array.length-1; i++){
 
             System.out.print(object.array[i].x + " ");
-            System.out.println(object.array[i].y + " ");
-            System.out.println(object.array[i].z + " ");
-            System.out.println(object.array[i].t + " ");
+            System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z + " ");
+            System.out.print(object.array[i].t + " ");
 
         }
-        System.out.println();
+
     }
     public static void main(String...args){
 
