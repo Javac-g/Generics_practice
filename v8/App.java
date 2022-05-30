235ac43c1530a3312b63d69596b3a47cd7cf1910
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 22:45:37 +0300

    6

--- v8/App.java
+++ v8/App.java
@@ -8,13 +8,45 @@ public class App {
             System.out.println(object.array[i].y + " ");
 
         }
+        System.out.println();
+    }
+
+    static void show_XYZ(Map<?extends xyz> object){
+        for (int i = 0; i <= object.array.length-1; i++){
+
+            System.out.print(object.array[i].x + " ");
+            System.out.println(object.array[i].y + " ");
+            System.out.println(object.array[i].z + " ");
+
+        }
+        System.out.println();
+    }
+    static void show_XYZT(Map<?extends xyzt> object){
+        for (int i = 0; i <= object.array.length-1; i++){
+
+            System.out.print(object.array[i].x + " ");
+            System.out.println(object.array[i].y + " ");
+            System.out.println(object.array[i].z + " ");
+            System.out.println(object.array[i].t + " ");
+
+        }
+        System.out.println();
     }
     public static void main(String...args){
 
 
 
+        xy[] two = {new xy(1,2), new xy(1,2), new xy(1,2)};
+        xyz[] three = {new xyz(1,2,3) , new xyz(1,2,3), new xyz(1,2,3)};
+        xyzt[] four = {new xyzt(1,2,3,4) , new xyzt(1,2,3,4), new xyzt(1,2,3,4)};
 
+        Map<xy> XY = new Map<>(two);
+        Map<xyz> XYZ = new Map<>(three);
+        Map<xyzt> XYZT = new Map<>(four);
 
+        show_XY(XY);
+        show_XYZ(XYZ);
+        show_XYZT(XYZT);
 
 
 
