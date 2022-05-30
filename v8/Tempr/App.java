f4d6b0701d156ce6d8917d71701a228dd20137de
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 23:08:58 +0300

    10

--- v8/Tempr/App.java
+++ v8/Tempr/App.java
@@ -1,4 +1,15 @@
 package v8.Tempr;
 
 public class App {
+
+    static <T> Integer run(Tool<T> tool , T[] vals, T t){
+        int c = 0;
+
+        for (int i = 0; i <= vals.length-1; i++){
+
+            if (tool.function(vals[i],t))c++;
+
+        }
+        return c;
+    }
 }
