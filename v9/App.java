6b8c854929fd30a01f8568089575777b96d5597e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-15 22:52:30 +0300

    m xy

--- v9/App.java
+++ v9/App.java
@@ -1,7 +1,15 @@
 package v9;
 
 public class App {
+    static void show_XY(MAP<? extends XY> object){
+        System.out.println(" --- [2 D] ---");
+        for (int i = 0; i <= object.array.length; i++ ){
+            System.out.print(object.array[i].x + " ");
+            System.out.print(object.array[i].y + " ");
+        }
+        System.out.println("\n");
 
+    }
     public static void main(String...args){
 
 
