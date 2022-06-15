a800fc80d11f5c804e87a45fa6bdfcf9e71146f3
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-15 23:01:02 +0300

    m app

--- v9/App.java
+++ v9/App.java
@@ -4,8 +4,9 @@ public class App {
     static void show_XY(MAP<? extends XY> object){
         System.out.println(" --- [2 D] ---");
         for (int i = 0; i <= object.array.length; i++ ){
-            System.out.print(object.array[i].x + " ");
-            System.out.print(object.array[i].y + " ");
+
+            System.out.print("[" + object.array[i].x + " ");
+            System.out.print(object.array[i].y + " ]");
         }
         System.out.println("\n");
 
@@ -13,15 +14,29 @@ public class App {
     static void show_XYZ(MAP<? extends XYZ> object){
         System.out.println(" --- [3 D] ---");
         for (int i = 0; i <= object.array.length; i++ ){
-            System.out.print(object.array[i].x + " ");
+            System.out.print("[" + object.array[i].x + " ");
             System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z + " ]");
+        }
+        System.out.println("\n");
+
+    }
+    static void show_XYZT(MAP<? extends XYZT> object){
+        System.out.println(" --- [4 D] ---");
+        for (int i = 0; i <= object.array.length; i++ ){
+            System.out.print("[" + object.array[i].x + " ");
             System.out.print(object.array[i].y + " ");
+            System.out.print(object.array[i].z + " ");
+            System.out.print(object.array[i].t + " ]");
         }
         System.out.println("\n");
 
     }
     public static void main(String...args){
 
+        XY[] A = {new XY(1,2),new XY(2,1),new XY(2,2)};
+        XYZ[] B = {new XYZ(1,2,3), new XYZ(3,2,3), new XYZ(3,3,3)};
+        XYZT[] C = {new XYZT(1, 2,3,4), new XYZT(4,3,2,1), new XYZT(4,4,4,4)};
 
 
 
