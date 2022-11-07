d4e7d5c7a82e0b031b844934d8fef0cb1ef86c70
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-07 20:52:10 +0200

    MAP

---
+++ V10/Map.java
@@ -0,0 +1,8 @@
+package V10;
+
+public class Map <T>{
+    T[] coords;
+    public Map(T[] coords){
+        this.coords = coords;
+    }
+}
---
+++ V10/XYZT.java
@@ -0,0 +1,9 @@
+package V10;
+
+public class XYZT extends XYZ{
+    int t;
+    public XYZT(int x,int y, int z,int t){
+        super(x,y,z);
+        this.t = t;
+    }
+}
