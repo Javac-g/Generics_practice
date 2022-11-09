464ce043698eac78c992b5ec8f2391fce53608a7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-10 00:17:50 +0200

    General class One

--- V10/GenCLass/Main.java
+++ V10/GenCLass/Main.java
@@ -1,8 +1,15 @@
 package V10.GenCLass;
 
 public class Main {
-
+    static <T> void process(One<T> c){
+        System.out.print(c.getValue() + " - ");
+        System.out.println(c.getValue().getClass().getName());
+    }
     public static void main(String...args){
+        One<String> str = new One<>("Hello");
+        One<Integer> num = new One<>(123);
+        process(str);
+        process(num);
 
     }
 }
--- V10/GenCLass/One.java
+++ V10/GenCLass/One.java
@@ -1,7 +1,11 @@
 package V10.GenCLass;
 
 public class One<T> {
+
     T value;
+    One(T value){
+        this.value = value;
+    }
     public T getValue(){
         return value;
     }
