f75d6cb237e3a014a3d7ba9c77f4ad0d922d56b2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-20 20:56:40 +0200

    XYZT extend XYZ class

---
+++ AllThemesFromStart/XYZT.java
@@ -0,0 +1,10 @@
+package AllThemesFromStart;
+
+public class XYZT extends XYZ{
+    protected int t;
+
+    public XYZT(int x, int y, int z, int t) {
+        super(x, y, z);
+        this.t = t;
+    }
+}
