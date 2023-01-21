e03ed39e978d7f38809591914987651b9ad9f2c3
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-21 21:31:52 +0200

    Generic interfaceces done

---
+++ AllThemesFromStart/GenInterfaces/One.java
@@ -0,0 +1,31 @@
+package AllThemesFromStart.GenInterfaces;
+
+public class One <T extends Comparable<T>> implements MinMax<T>{
+    private T[] vals;
+
+    public One(T[] vals) {
+        this.vals = vals;
+    }
+
+    @Override
+    public T min() {
+        T v = vals[0];
+        for (int i = 0; i < vals.length;i++){
+            if (vals[i].compareTo(v) < 0){
+                v = vals[i];
+            }
+        }
+        return v;
+    }
+
+    @Override
+    public T max() {
+        T v = vals[0];
+        for (int i =0; i < vals.length; i++){
+            if (vals[i].compareTo(v) > 0){
+                v = vals[i];
+            }
+        }
+        return v;
+    }
+}
