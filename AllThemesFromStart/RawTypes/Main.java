e14a80b49394df6d6770d4475da0c330ac692e96
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-21 21:49:38 +0200

    raw types done

---
+++ AllThemesFromStart/RawTypes/Main.java
@@ -0,0 +1,15 @@
+package AllThemesFromStart.RawTypes;
+
+public class Main {
+    public static void main(String...args){
+
+        One<Integer> one = new One<>(234);
+        One<String> two = new One<>("Hello");
+        One raw = new One(22.4);
+
+        Integer i = one.getOb();
+        String s = two.getOb();
+        Double d = (Double) raw.getOb();//require type bounding to extract value.
+        int ir = (Integer) raw.getOb(); //Throws error.
+    }
+}
