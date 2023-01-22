e44886f4866f39ec920878dfd5900ea2dbbe8a87
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-23 00:07:53 +0200

    Generic hierarchy comparing

---
+++ AllThemesFromStart/GenHierarchyComparing/One.java
@@ -0,0 +1,11 @@
+package AllThemesFromStart.GenHierarchyComparing;
+
+public class One<T> {
+    protected T t;
+    public One(T t){
+        this.t = t;
+    }
+    T getT(){
+        return t;
+    }
+}
---
+++ AllThemesFromStart/GenHierarchyComparing/Two.java
@@ -0,0 +1,7 @@
+package AllThemesFromStart.GenHierarchyComparing;
+
+public class Two <T> extends One<T>{
+    public Two(T t){
+        super(t);
+    }
+}
