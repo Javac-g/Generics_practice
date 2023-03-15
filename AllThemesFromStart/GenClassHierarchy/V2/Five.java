d07d6c129fd7a9fe731a7127e95d5b80b1801365
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-16 00:37:03 +0200

    Generic class Hierarchy - child  class

---
+++ AllThemesFromStart/GenClassHierarchy/V2/Five.java
@@ -0,0 +1,12 @@
+package AllThemesFromStart.GenClassHierarchy.V2;
+
+public class Five <T> extends FourNG{
+    private  T ob;
+    public Five(T ob,int i){
+        super(i);
+        this.ob = ob;
+    }
+    public T getOb(){
+        return ob;
+    }
+}
---
+++ AllThemesFromStart/GenClassHierarchy/V2/FourNG.java
@@ -0,0 +1,11 @@
+package AllThemesFromStart.GenClassHierarchy.V2;
+
+public class FourNG {
+    private int num;
+    public FourNG(int num){
+        this.num = num;
+    }
+    public int getNum(){
+        return num;
+    }
+}
