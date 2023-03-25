0d2564480bf1cf80ffb026c5f5dd930d40fa007a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-25 18:02:18 +0200

    XY class

---
+++ AllThemesFromStart/LimitedWilcards/V4/XY.java
@@ -0,0 +1,19 @@
+package AllThemesFromStart.LimitedWilcards.V4;
+
+public class XY {
+    private final Integer x;
+    private final Integer y;
+
+    public XY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX(){
+        return x;
+    }
+
+    public Integer getY(){
+        return y;
+    }
+}
