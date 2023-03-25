97ecd19db41e45b7ad798752edc2ca08b56bf534
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-25 18:54:57 +0200

    Limited wildcards done

--- AllThemesFromStart/LimitedWilcards/V4/Map.java
+++ AllThemesFromStart/LimitedWilcards/V4/Map.java
@@ -1,6 +1,8 @@
 package AllThemesFromStart.LimitedWilcards.V4;
 
 public class Map <T extends XY>{
+    //Can create Map only with XY or his subclass Type
+    //Using this class for coordinates encapsulation in XY,XYZ,XYZT objects
     private final T[] data;
 
     public Map(T[] data){
