cc0f8afc8baa8aa9f146af9f55fb0d0873a2cdf5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-25 20:51:17 +0200

    Generic subclass - class hierarchy

---
+++ AllThemesFromStart/GenClassHierarchy/V4/GenSubClass/Main.java
@@ -0,0 +1,10 @@
+package AllThemesFromStart.GenClassHierarchy.V4.GenSubClass;
+
+public class Main {
+    public static void main(String... args) {
+        One one = new One(1234);
+        Two<Double> two = new Two<>(22.4);
+
+        two.show();
+    }
+}
---
+++ AllThemesFromStart/GenClassHierarchy/V4/GenSubClass/Two.java
@@ -0,0 +1,17 @@
+package AllThemesFromStart.GenClassHierarchy.V4.GenSubClass;
+
+public class Two <T extends Number> extends One{
+    private final T ob;
+
+    public Two(T ob){
+        super(ob.intValue());
+        this.ob = ob;
+    }
+    public T getOb(){
+        return ob;
+    }
+    public void show(){
+        System.out.println("Num value: " + getNum());
+        System.out.println("T value: " + getOb());
+    }
+}
