ff04388e8bfc006456491704a1fa7cd51fb5a3b7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-25 20:25:16 +0200

    Raw types done

---
+++ AllThemesFromStart/RawTypes/V4/One.java
@@ -0,0 +1,11 @@
+package AllThemesFromStart.RawTypes.V4;
+
+public class One <T>{
+    private final T ob;
+    public One(T ob){
+        this.ob = ob;
+    }
+    public T getOb(){
+        return ob;
+    }
+}
