3ecefd3f351fcdeb3155e01cc66508e5cee388be
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-03-25 21:36:26 +0200

    Generics done

---
+++ AllThemesFromStart/ArraysErrors/Main.java
@@ -0,0 +1,8 @@
+package AllThemesFromStart.ArraysErrors;
+
+public class Main {
+    public static void main(String[] args) {
+        //Human<Integer>[] humans = new Human<Integer>[3]; wrong
+        Human<?>[] humans = new Human<?>[3];//right
+    }
+}
