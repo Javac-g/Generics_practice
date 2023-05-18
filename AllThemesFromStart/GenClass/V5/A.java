b21a788b2c0cfa637a9dd5a0c7036f7fbdd8faa2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-05-19 00:13:40 +0300

    started

---
+++ AllThemesFromStart/GenClass/V5/A.java
@@ -0,0 +1,11 @@
+package AllThemesFromStart.GenClass.V5;
+
+public class A <T>{
+    private final T ob;
+    public A (T ob){
+        this.ob = ob;
+    }
+    public T getOb(){
+        return ob;
+    }
+}
